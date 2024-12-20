769a4dd3ab82ef515e0af1673070b1f17d2f7a2e
lucashurt <lucashurtado814@gmail.com>
2024-12-19 19:27:32 -0500

    added profile views wher clicking on the username of any poster will redirect you to their page where you see their posts, also added an ability to follow users and see your following users posts exclusively in a seperate tab

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -2,25 +2,29 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("#all_button").addEventListener("click", () => load_posts("All"))
     document.querySelector("#following_button").addEventListener("click", () => load_posts("Following"))
     document.querySelector("#submit_post").addEventListener("click", submit_post)
+    document.querySelector("#user_profile").addEventListener("click", () => load_profile(document.querySelector("#user_profile").innerText))
 
     load_posts("All");
 })
 
 function load_posts(posts) {
     event.preventDefault()
+
     document.querySelector("#Posts").style.display = "block";
+    document.querySelector("#heading").style.display = "block";
     document.querySelector("#Create").style.display = "none";
     document.querySelector("#Profile").style.display = "none";
 
 
     const postsContainer = document.querySelector("#Posts");
-    postsContainer.innerHTML = ""; // Clears any previous posts
+    postsContainer.innerHTML = "";
 
-    document.querySelector("#Posts").innerHTML = `<h1>${posts}</h1>`;
 
+    document.querySelector("#heading").innerHTML = `<h1>${posts}</h1>`;
     if(posts ==="All") {
         document.querySelector("#Create").style.display = "block";
     }
+
     fetch(`/posts/${posts}`)
         .then(res => res.json())
             .then(posts => {
@@ -36,6 +40,7 @@ function load_posts(posts) {
                                  <button class = "btn btn-sm btn-outline-primary">Like </button> ${post.likes}
                                  </div>
                                  </div>`
+                    element.querySelector("#profile_button").addEventListener("click", () => load_profile(post.author));
             postsContainer.appendChild(element);
         })
     })
@@ -60,10 +65,55 @@ function submit_post() {
 }
 
 function load_profile(username){
-    event.preventDefault()
     document.querySelector("#Create").style.display = "none";
     document.querySelector("#Posts").style.display = "none";
+    document.querySelector("#heading").style.display = "none";
     document.querySelector("#Profile").style.display = "block";
+    document.querySelector("#username_display").innerText = username;
 
-    document.querySelector("h1").innerHTML = username;
+    postsContainer = document.querySelector("#profile_posts");
+    postsContainer.innerHTML = ""
+
+
+    fetch(`/profile/${username}`)
+    .then(response => response.json())
+     .then(user => {
+
+         if(user.is_following){
+            document.querySelector("#follow_button").innerText = "Unfollow";
+            document.querySelector("#follow_button").onclick = ()=> unfollow_user(username)
+         }
+         else{
+             document.querySelector("#follow_button").innerText = "Follow";
+             document.querySelector("#follow_button").onclick = ()=> follow_user(username)
+        }
+         document.querySelector("#followers_label").innerText = `Followers:${user.followers} Following:${user.following}`
+     })
+
+    fetch(`/profileposts/${username}`)
+    .then(res => res.json())
+    .then(posts => {
+        posts.forEach(post => {
+                    const element = document.createElement("div");
+                    element.innerHTML =`
+                                 <div class="border" style="margin:1%">
+                                 <div id = "profile_button" style="margin: 10px">
+                                 <button class ="btn btn-light"><strong>${post.author}</strong></button>
+                                 <p>${post.body}</p>
+                                 <p style = "color:grey">${post.timestamp}</p>
+                                 <button class = "btn btn-sm btn-outline-primary">Like </button> ${post.likes}
+                                 </div>
+                                 </div>`
+            postsContainer.appendChild(element);
+        })
+    })
+}
+
+function follow_user(username) {
+    fetch(`/follow/${username}`)
+        .then(() => {load_profile(username)})
+}
+function unfollow_user(username) {
+    fetch(`/unfollow/${username}`)
+        .then(() => {load_profile(username)})
 }
